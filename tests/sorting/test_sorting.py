b38161f3be145327724f182c239c2471652207ec
Brayan Santos <brayan.santosrosa@gmail.com>
2023-02-17 18:24:12 -0300

    Requisito 12 concluido

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,15 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+from datetime import date
 
 
 def test_sort_by_criteria():
-    pass
+    jobs = [
+        {"min_salary": 10, "max_salary": 11, "date_posted": date.today()},
+        {"min_salary": 20, "max_salary": 30, "date_posted": date.today()},
+        ]
+    sort_by(jobs, 'max_salary')
+    assert jobs[0] == ({
+        "min_salary": 20,
+        "max_salary": 30,
+        "date_posted": date.today()
+    })
