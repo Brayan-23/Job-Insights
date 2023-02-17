939375345b7002baca16b9c5d107ab4e86daa39e
Brayan Santos <brayan.santosrosa@gmail.com>
2023-02-17 16:53:21 -0300

    Requisito 10 concluido

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,6 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    'Recebendo uma string qualquer deve retornar o número de letras'
+    assert count_ocurrences('data/jobs.csv', 'Time') == 7331
